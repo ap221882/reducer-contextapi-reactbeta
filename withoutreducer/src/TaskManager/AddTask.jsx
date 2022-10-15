b3e3dbc809720358412bfbd06efa6ca5601fd3a6
ajayp <ajayp@naehas.com>
2022-10-15 20:03:22 +0530

    application with useState only

---
+++ withoutreducer/src/TaskManager/AddTask.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+
+const AddTask = ({ onAddTask }) => {
+  const [text, setText] = useState("")
+  return (
+    <>
+      <input
+        placeholder='Add Task'
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value)
+        }}
+      />
+      <button
+        onClick={() => {
+          setText("")
+          onAddTask(text)
+        }}
+      >
+        Add
+      </button>
+    </>
+  )
+}
+
+export default AddTask
