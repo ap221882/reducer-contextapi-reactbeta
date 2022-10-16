b0742dc04131536f90fa1c837254ff9cedf09eac
ajay <ap221882@gmail.com>
2022-10-16 12:18:13 +0530

    Contexts are now provided!

--- withreducer/src/TaskManager/TaskManager.jsx
+++ withreducer/src/TaskManager/TaskManager.jsx
@@ -1,49 +1,16 @@
 import React, { useReducer } from "react"
 import AddTask from "./AddTask"
 import TaskList from "./TaskList"
-import { tasksReducer } from "../reducers"
-import { TasksContext, TasksDispatchContext } from "../contexts/TasksContext"
+import TasksProvider from "../contexts/TasksProvider"
 
 export const TaskManager = () => {
-  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks)
-  function handleAddTask(text) {
-    dispatch({
-      type: "added",
-      id: nextId++,
-      text: text,
-    })
-  }
-
-  function handleChangeTask(task) {
-    dispatch({
-      type: "changed",
-      task: task,
-    })
-  }
-
-  function handleDeleteTask(taskId) {
-    dispatch({
-      type: "deleted",
-      id: taskId,
-    })
-  }
-
   return (
     <>
-      <TasksContext.Provider value={tasks}>
-        <TasksDispatchContext.Provider value={dispatch}>
-          <h1>Todo application</h1>
-          <AddTask />
-          <TaskList />
-        </TasksDispatchContext.Provider>
-      </TasksContext.Provider>
+      <TasksProvider>
+        <h1>Todo application</h1>
+        <AddTask />
+        <TaskList />
+      </TasksProvider>
     </>
   )
 }
-
-let nextId = 3
-const initialTasks = [
-  { id: 0, text: "Visit Kafka Museum", done: true },
-  { id: 1, text: "Watch a puppet show", done: false },
-  { id: 2, text: "Lennon Wall pic", done: false },
-]
---
+++ withreducer/src/contexts/TasksProvider.jsx
@@ -0,0 +1,22 @@
+import React, { useReducer } from "react"
+import { tasksReducer } from "../reducers"
+import { TasksContext, TasksDispatchContext } from "./TasksContext"
+
+const TasksProvider = ({ children }) => {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks)
+  return (
+    <TasksContext.Provider value={tasks}>
+      <TasksDispatchContext.Provider
+        value={dispatch}
+      ></TasksDispatchContext.Provider>
+      {children}
+    </TasksContext.Provider>
+  )
+}
+
+export default TasksProvider
+const initialTasks = [
+  { id: 0, text: "Visit Kafka Museum", done: true },
+  { id: 1, text: "Watch a puppet show", done: false },
+  { id: 2, text: "Lennon Wall pic", done: false },
+]
